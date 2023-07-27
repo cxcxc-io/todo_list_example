de59df2bb64412a855601026782ac0e891dbeb40
unknown <sheiyuray@gmail.com>
2023-07-27 20:02:37 +0800

    更新註解

--- lib/controllers/platform_controller.dart
+++ lib/controllers/platform_controller.dart
@@ -1,4 +1,4 @@
 /// 判斷用戶是否為網頁應用，導流到不同的 view。
 ///
-/// 若為網頁，導流到 [WebView]；若非網頁，導流到 [MobileView]
+/// 在 build widget 內部判斷：在若為網頁，導流到 [WebView]；若非網頁，導流到 [MobileView]
 class PlatformController extends StatelessWidget {}
